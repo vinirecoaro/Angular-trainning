bb5a0fc7a621924209c42ad44a7c02603e35c130
Vinicius Recoaro <vrecoaro@hotmail.com>
2024-06-11 21:34:51 -0300

    Added page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
+import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 const routes : Routes = [
-  {path: "primeira-pagina", component: PrimeiraPaginaComponent}
+  {path: "primeira-pagina", component: PrimeiraPaginaComponent},
+  {path: "segunda-pagina", component: SegundaPaginaComponent},
+  {path: "", redirectTo:"primeira-pagina", pathMatch:"full"},
+  {path: "**", component: PaginaNaoEncontradaComponent}
 ]
 
 @NgModule({
