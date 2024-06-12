fb1e778b66473efab7b813eac27a7a3bdd09efc0
Vinicius Recoaro <vrecoaro@hotmail.com>
2024-06-11 21:50:21 -0300

    route params ans query params

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
 
 const routes : Routes = [
   {path: "primeira-pagina", component: PrimeiraPaginaComponent},
   {path: "segunda-pagina", component: SegundaPaginaComponent},
   {path: "", redirectTo:"primeira-pagina", pathMatch:"full"},
+  {path: "pagina-com-parametros/:id", component: PaginaComParametrosComponent},
   {path: "**", component: PaginaNaoEncontradaComponent}
 ]
 
