78e760a7df72ba57496a8c557cbded9f805f4508
Vinicius Recoaro <vrecoaro@hotmail.com>
2024-06-13 20:17:23 -0300

    Router guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,18 @@ import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.compo
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
+import { PaginaProtegidaComponent } from './pagina-protegida/pagina-protegida.component';
+import { AuthGuard } from './auth.guard';
+import { LoginComponent } from './login/login.component';
 
 const routes : Routes = [
   {path: "primeira-pagina", component: PrimeiraPaginaComponent},
   {path: "segunda-pagina", component: SegundaPaginaComponent},
-  {path: "", redirectTo:"primeira-pagina", pathMatch:"full"},
   {path: "pagina-com-parametros/:id", component: PaginaComParametrosComponent},
   {path: 'lazy-loading', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) },
+  {path: "pagina-protegida", component: PaginaProtegidaComponent, canActivate:[AuthGuard]},
+  {path: "login", component: LoginComponent},
+  {path: "", redirectTo:"primeira-pagina", pathMatch:"full"},
   {path: "**", component: PaginaNaoEncontradaComponent}
 ]
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.componen
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
 import { PaginaProtegidaComponent } from './pagina-protegida/pagina-protegida.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,11 +18,13 @@ import { PaginaProtegidaComponent } from './pagina-protegida/pagina-protegida.co
     SegundaPaginaComponent,
     PaginaNaoEncontradaComponent,
     PaginaComParametrosComponent,
-    PaginaProtegidaComponent
+    PaginaProtegidaComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
